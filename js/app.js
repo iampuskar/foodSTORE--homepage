9c0849c9b547e9e93d878720d707342f4bc87a41
PuskarGhimire <puskar93@gmail.com>
2021-11-17 12:14:43 +0545

    Final file uploaded

---
+++ js/app.js
@@ -0,0 +1,32 @@
+$(document).ready(function(){
+  $('.food-slider').slick({
+   slidesToShow:3,
+   slidesToScroll:1,
+   autoplay:3000,
+   prevArrow:".prev-btn",
+   nextArrow:".next-btn",
+   responsive:[
+   {
+    breakpoint:992,
+    settings:{
+      slidesToShow:2,
+    }
+   },
+   {
+    breakpoint:768,
+    settings:{
+      slidesToShow:1,
+    }
+   }
+   ]
+
+  });
+
+
+
+$('.nav-trigger').click(function() {
+  
+  $('.site-content-wrapper').toggleClass('scaled');
+})
+
+});
